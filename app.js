a37d173fb65ac03001fe5c25a66ac20fa12a9590
lluvio <jyili@yahoo.com>
2015-06-02 20:29:44 +0800

    be example

--- app.js
+++ app.js
@@ -6,68 +6,38 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-var user_register = require('./user/userRegister');
-var user_mayChange = require('./user/mayChange');
-var user_getNewName = require('./user/getNewName');
-var user_doChange = require('./user/doChange');
-var user_verify = require('./user/verify');
-//all module are promise
+var user_register = require('./userRegister');
+
+var doUnit = require('./doUnit');
+
 
 co(function*(){
 
 	var register = yield co(user_register);
 
-	var mayChange = yield co(user_mayChange);
+	router.get('/:dir/:file',function(req,res){
+				// res.send(unescape(result[req.name]));
+				var pathname = __dirname + '/doc/'+req.params.dir+'/'+req.params.file+'.yml';
 
-	var getNewName = yield co(user_getNewName);
+				console.log(pathname);
 
-	var doChange = yield co(user_doChange);
+				//通过路由读取yml用例
+				var result = co(doTest(pathname)).then(function(data){
 
-	var verify = yield co(user_verify);
-
-	var result = {
-			register  : register,
-			mayChange : mayChange,
-			getNewName: getNewName,
-			doChange 	: doChange,
-			verify		: verify
-	};
-
-	router.param('name', function(req,res,next,name){
-			// res.send(name);
-			// console.log('nodemon');
-			req.name = name;
-			next();
-	});
-
-	router.get('/user/:name',function(req,res){
-				res.send(unescape(result[req.name]));
+						console.log(data);
+						//将结果返回
+						res.send(data);
+				});
 	})
 
 	app.use('/',router);
 
 	console.log('register: '.green + register);
-	console.log('mayChange: '.green + mayChange);
-	console.log('getNewName: '.green + getNewName);
-	console.log('doChange: '.green + doChange);
-	console.log('verify: '.green + verify);
+	// console.log('mayChange: '.green + mayChange);
+	// console.log('getNewName: '.green + getNewName);
+	// console.log('doChange: '.green + doChange);
+	// console.log('verify: '.green + verify);
 
-	// log.debug('test');
-	// var user = yield {
-	// 	register : co(user_register),
-	// 	mayChange : co(user_mayChange),
-	// 	getNewName : co(user_getNewName),
-	// 	doChange : co(user_doChange),
-	// 	verify : co(user_verify)
-	// };
-	//
-	// // console.log(user);
-	//
-	// console.log('register: '.green + user.register);
-	// console.log('mayChange: '.green + user.mayChange);
-	// console.log('getNewName: '.green + user.getNewName);
-	// console.log('doChange: '.green + user.doChange);
-	// console.log('verify: '.green + user.verify);
 }).catch(onerror);
 
 
@@ -75,7 +45,6 @@ function jsonToObj(data){
 	var result;
 
 	if(!(result=JSON.parse(data)))console.error('can not instance json');
-
 	return result;
 }
 
@@ -86,10 +55,9 @@ function onerror(err) {
   console.error(err.stack);
 }
 
-
 app.listen(4000);
 
-console.log('..................[nodemon] starting  '.green+'localhost at 4000'.red);
+console.log('..................[listen] starting  '.green+'localhost at 4000'.red);
 
 // co(user_register).then(function(data){
 // 		console.log(data);
